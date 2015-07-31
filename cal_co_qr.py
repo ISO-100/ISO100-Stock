bbd7ed3658a8eac6876abdbcddf9a2bb6680ad5c
Cong <cong.lang@qq.com>
2015-07-31 20:44:42 +0800

    update

--- cal_co_qr.py
+++ cal_co_qr.py
@@ -7,6 +7,7 @@ import urllib
 import sqlite3
 import time 
 
+start_time = time.time()
 ltime=time.time()-86400
 tYear=time.strftime("%Y", time.localtime(ltime))
 #print tYear
@@ -17,7 +18,7 @@ conn = sqlite3.connect('stock.sqlite')
 c = conn.cursor()
 c.execute("DROP TABLE IF EXISTS cal_co_qr")
 
-#字段说明示例：
+#e.g.
 #R_2014_1Q: 季报营收
 #P_2014_1Q：季报利润
 #P_rate_1Q：利润率
@@ -27,34 +28,18 @@ c.execute("DROP TABLE IF EXISTS cal_co_qr")
 #lastyear_yoy：季报利润增加同比
 #forecast_PE：预测PE
 #target_price：目标价位
-str_sql = '''CREATE TABLE cal_co_qr(
+creat_sql = '''CREATE TABLE cal_co_qr(
                stk_num CHAR PRIMARY KEY,
                stk_name CHAR,
-               %s NUM,
-               %s NUM,
-               P_rate_1Q NUM,
-               %s NUM,
-               %s NUM,
-               P_rate_2Q NUM,
-               %s NUM,
-               %s NUM,
-               P_rate_3Q NUM,
-               %s NUM,
-               %s NUM,
-               P_rate_4Q NUM,
-               %s NUM,
-               %s NUM,
-               P_rate_5Q NUM,
-               %s NUM,
-               %s NUM,
-               P_rate_6Q NUM,
-               %s NUM,
-               %s NUM,
-               P_rate_7Q NUM,
-               %s NUM,
-               %s NUM,
-               %s NUM,
-               %s NUM,
+               %s NUM,%s NUM,P_rate_1Q NUM,
+               %s NUM,%s NUM,P_rate_2Q NUM,
+               %s NUM,%s NUM,P_rate_3Q NUM,
+               %s NUM,%s NUM,P_rate_4Q NUM,
+               %s NUM,%s NUM,P_rate_5Q NUM,
+               %s NUM,%s NUM,P_rate_6Q NUM,
+               %s NUM,%s NUM,P_rate_7Q NUM,
+               %s NUM,%s NUM,
+               %s NUM,%s NUM,
                forecast_EPS NUM,
                lastyear_yoy NUM,
                forecast_PE NUM,
@@ -69,13 +54,94 @@ str_sql = '''CREATE TABLE cal_co_qr(
                      "FR_"+tYear+"_4Q","FP_"+tYear+"_4Q",
                      "R_"+tYear+"_4Q","P_"+tYear+"_4Q")
 
-c.execute(str_sql)
+#Creat table 'cal_co_qr'
+c.execute(creat_sql)
 
+def query_lst(arg1,arg2,arg3):
+    dd = conn.cursor()
+    dd.execute("SELECT operation_revenue,net_profit from CO_QR WHERE stk_num=%s AND QUARTER='%s'"% (arg1,arg2+arg3))
+    rows = dd.fetchall()
+    return rows
 
-#for stk_num in c.execute("SELECT stk_num from stk_lst"):
-    #stk_num = stk_num[0]
+    
+for stk_lst in c.execute("SELECT stk_num,stk_name from stk_lst"):
+    stk_num = stk_lst[0]
+    stk_name = stk_lst[1]
+    #print stk_num
+    R_lYear_1Q = P_lYear_1Q = ""
+    R_lYear_2Q = P_lYear_2Q = ""
+    R_lYear_3Q = P_lYear_3Q = ""
+    R_lYear_4Q = P_lYear_4Q = ""
+    R_tYear_1Q = P_tYear_1Q = ""
+    R_tYear_2Q = P_tYear_2Q = ""
+    R_tYear_3Q = P_tYear_3Q = ""
+    R_tYear_4Q = P_tYear_4Q = ""
 
+    temp = query_lst(stk_num,lYear,"-1Q")
+    if len(temp)>0:
+        R_lYear_1Q = temp[0][0]
+        P_lYear_1Q = temp[0][1]
+    else:
+        R_lYear_1Q = "N/A"
+        P_lYear_1Q = "N/A"
+    print R_lYear_1Q, P_lYear_1Q
+        
+    temp = query_lst(stk_num,lYear,"-2Q")
+    if len(temp)>0:
+        R_lYear_2Q = temp[0][0]
+        P_lYear_2Q = temp[0][1]
+    else:
+        R_lYear_2Q = "N/A"
+        P_lYear_2Q = "N/A"
 
+    temp = query_lst(stk_num,lYear,"-3Q")
+    if len(temp)>0:
+        R_lYear_3Q = temp[0][0]
+        P_lYear_3Q = temp[0][1]
+    else:
+        R_lYear_3Q = "N/A"
+        P_lYear_3Q = "N/A"
+
+    temp = query_lst(stk_num,lYear,"-4Q")
+    if len(temp)>0:
+        R_lYear_4Q = temp[0][0]
+        P_lYear_4Q = temp[0][1]
+    else:
+        R_lYear_4Q = "N/A"
+        P_lYear_4Q = "N/A"
+
+    temp = query_lst(stk_num,tYear,"-1Q")
+    if len(temp)>0:
+        R_tYear_1Q = temp[0][0]
+        P_tYear_1Q = temp[0][1]
+    else:
+        R_tYear_1Q = "N/A"
+        P_tYear_1Q = "N/A"
+
+    temp = query_lst(stk_num,tYear,"-2Q")
+    if len(temp)>0:
+        R_tYear_2Q = temp[0][0]
+        P_tYear_2Q = temp[0][1]
+    else:
+        R_tYear_2Q = "N/A"
+        P_tYear_2Q = "N/A"
+
+    temp = query_lst(stk_num,tYear,"-3Q")
+    if len(temp)>0:
+        R_tYear_3Q = temp[0][0]
+        P_tYear_3Q = temp[0][1]
+    else:
+        R_tYear_3Q = "N/A"
+        P_tYear_3Q = "N/A"
+
+    temp = query_lst(stk_num,tYear,"-4Q")
+    if len(temp)>0:
+        R_tYear_4Q = temp[0][0]
+        P_tYear_4Q = temp[0][1]
+    else:
+        R_tYear_4Q = "N/A"
+        P_tYear_4Q = "N/A"
 
 c.close()
 print "Process Done."
+print("--- This program costs %0.2f seconds ---" % (time.time() - start_time))
