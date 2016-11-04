c8338589efe2aaed985de9dafe8ad1b8f642b56d
teashaw <xiaopeiqing@gmail.com>
2016-11-04 10:21:15 +0000

    update rst_stk_flt

--- rst_stk_flt.py
+++ rst_stk_flt.py
@@ -16,6 +16,8 @@ def main():
     c.execute('''CREATE TABLE IF NOT EXISTS rst_stk_flt(
                 stk_num CHAR(20) PRIMARY KEY,
                 stk_name CHAR(20),
+                mid_value float(10,2),
+                target_price_range float(10,2),
                 eps_y16_evg float(5,2),
                 y15_to_y16_growth float(6,4),
                 org_predict_pe float(10,2),
@@ -31,20 +33,40 @@ def main():
         if not (stk_num.startswith("6") or stk_num.startswith("0")): continue
         stock[stk_num] = stk_name
         stk_name = stock[stk_num]
+        
+        #Initialize 
+        eps_y16_evg = y15_to_y16_growth = org_predict_pe = org_predict_peg = target_price = mid_value = target_price_range = "N/A"
+        
         c.execute("SELECT eps_y16_evg,y15_to_y16_growth,org_predict_pe,org_predict_peg,target_price from cal_int_eps where stk_num = %s", (stk_num,))
         fetch = c.fetchone()
-        print fetch
+        
         if not fetch is None: 
             eps_y16_evg = fetch[0]
             y15_to_y16_growth = fetch[1]
             org_predict_pe = fetch[2]
             org_predict_peg = fetch[3]
             target_price = fetch[4]
-            c.execute('''REPLACE INTO rst_stk_flt (
-                    stk_num,stk_name,eps_y16_evg,y15_to_y16_growth,org_predict_pe,org_predict_peg,target_price)
-                    VALUES (%s,%s,%s,%s,%s,%s,%s)''',
-                    (stk_num,stk_name,eps_y16_evg,y15_to_y16_growth,org_predict_pe,org_predict_peg,target_price))
-            print stk_num,stk_name,eps_y16_evg,y15_to_y16_growth,org_predict_pe,org_predict_peg,target_price
+            
+            print "eps_y16_evg", eps_y16_evg
+            print "y15_to_y16_growth", y15_to_y16_growth
+            print "org_predict_pe", org_predict_pe
+            print "org_predict_peg", org_predict_peg
+            print "target_price", target_price
+            
+        c.execute("SELECT mid_value, target_price_range from cal_co_er where stk_num = %s", (stk_num,))
+        fetch = c.fetchone()
+        
+        if not fetch is None:
+            mid_value = fetch[0]
+            target_price_range = fetch[1]
+            print "mid_value", mid_value
+            print "target_price_range", target_price_range
+            
+        c.execute('''REPLACE INTO rst_stk_flt (
+                stk_num,stk_name,mid_value,target_price_range,eps_y16_evg,y15_to_y16_growth,org_predict_pe,org_predict_peg,target_price)
+                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)''',
+                (stk_num,stk_name,mid_value,target_price_range,eps_y16_evg,y15_to_y16_growth,org_predict_pe,org_predict_peg,target_price))
+        print stk_num,stk_name,eps_y16_evg,y15_to_y16_growth,org_predict_pe,org_predict_peg,target_price
     con.commit()    
     c.close()
     
