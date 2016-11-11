43f71093df85a6e297d3e38384a127a3cb8db333
teashaw <xiaopeiqing@gmail.com>
2016-11-11 10:40:05 +0000

    added cal_co_qr

--- cal_co_qr.py
+++ cal_co_qr.py
@@ -2,20 +2,28 @@
 #Update on 2015/08/04
 #Version 2.0
 #By Cong
+#Last modified by Teague Xiao on 20161107
 
-import urllib
-import sqlite3
-import time 
+
+from datetime import date
+import MySQLdb
+import os
+import ConfigParser
+import time
 
 start_time = time.time()
-ltime=time.time()-86400
-tYear=time.strftime("%Y", time.localtime(ltime))
+#ltime=time.time()-86400
+#tYear=time.strftime("%Y", time.localtime(ltime))
 #print tYear
-lYear="%i"%(int(tYear)-1)
+#lYear="%i"%(int(tYear)-1)
 #print lYear
+tYear = str(date.today().year)
+lYear = str(date.today().year - 1)
 
-conn = sqlite3.connect('stock.sqlite')
-c = conn.cursor()
+Config = ConfigParser.ConfigParser()
+Config.read("settings.ini")
+con = MySQLdb.connect( Config.get('mysql', 'host'), Config.get('mysql', 'username'), Config.get('mysql', 'password'), Config.get('mysql', 'DB'), charset="utf8" )
+c = con.cursor()
 c.execute("DROP TABLE IF EXISTS cal_co_qr")
 
 #e.g.
@@ -28,23 +36,23 @@ c.execute("DROP TABLE IF EXISTS cal_co_qr")
 #lastyear_yoy：季报利润增加同比
 #forecast_PE：预测PE
 #target_price：目标价位
-creat_sql = '''CREATE TABLE cal_co_qr(
-               stk_num CHAR PRIMARY KEY,
-               stk_name CHAR,
-               %s NUM,%s NUM,P_rate_1Q NUM,
-               %s NUM,%s NUM,P_rate_2Q NUM,
-               %s NUM,%s NUM,P_rate_3Q NUM,
-               %s NUM,%s NUM,P_rate_4Q NUM,
-               %s NUM,%s NUM,P_rate_5Q NUM,
-               %s NUM,%s NUM,P_rate_6Q NUM,
-               %s NUM,%s NUM,P_rate_7Q NUM,
-               %s NUM,%s NUM,
-               %s NUM,%s NUM,
-               forecast_EPS NUM,
-               lastyear_yoy NUM,
-               forecast_PE NUM,
-               target_price NUM
-               )'''%("R_"+lYear+"_1Q","P_"+lYear+"_1Q",
+creat_sql = '''CREATE TABLE IF NOT EXISTS cal_co_qr(
+               stk_num CHAR(20) PRIMARY KEY,
+               stk_name CHAR(20),
+               %s float,%s float,P_rate_1Q float,
+               %s float,%s float,P_rate_2Q float,
+               %s float,%s float,P_rate_3Q float,
+               %s float,%s float,P_rate_4Q float,
+               %s float,%s float,P_rate_5Q float,
+               %s float,%s float,P_rate_6Q float,
+               %s float,%s float,P_rate_7Q float,
+               %s float,%s float,
+               %s float,%s float,
+               forecast_EPS float,
+               lastyear_yoy float,
+               forecast_PE float,
+               target_price float
+               )''' %("R_"+lYear+"_1Q","P_"+lYear+"_1Q",
                      "R_"+lYear+"_2Q","P_"+lYear+"_2Q",
                      "R_"+lYear+"_3Q","P_"+lYear+"_3Q",
                      "R_"+lYear+"_4Q","P_"+lYear+"_4Q",
@@ -58,8 +66,9 @@ creat_sql = '''CREATE TABLE cal_co_qr(
 c.execute(creat_sql)
 
 def query_lst(arg1,arg2,arg3):
-    dd = conn.cursor()
-    dd.execute("SELECT operation_revenue,net_profit from CO_QR WHERE stk_num=%s AND QUARTER='%s'"% (arg1,arg2+arg3))
+    dd = con.cursor()
+    print arg1,arg2,arg3
+    dd.execute("SELECT operation_revenue,net_profit from co_qr WHERE stk_num=%s AND quarter=%s", (arg1,arg2+arg3,))
     rows = dd.fetchall()
     list = [1,2,3]
     if len(rows)>0:
@@ -71,17 +80,22 @@ def query_lst(arg1,arg2,arg3):
         list[0] = "N/A"
         list[1] = "N/A"
         list[2] = "N/A"
+        
+    print list[0],list[1],list[2]
     return list
 
 stk_cpt_check = ""
 stk_idt_check = ""
 print "It will takes few mins, please wait..."
     
-for stk_lst in c.execute("SELECT stk_num,stk_name from stk_lst"):
+c.execute("SELECT stk_num,stk_name from stk_lst")
+
+for stk_lst in c.fetchall():
 
     #stk_lst = ["202003","RC003"]
     stk_num = stk_lst[0]
     stk_name = stk_lst[1]
+    print stk_num, stk_name
     #print stk_num
     R_lYear_1Q = P_lYear_1Q = P_rate_1Q = ""
     R_lYear_2Q = P_lYear_2Q = P_rate_2Q = ""
@@ -136,9 +150,9 @@ for stk_lst in c.execute("SELECT stk_num,stk_name from stk_lst"):
     P_rate_8Q=list_temp[2]
 
 #'stk_cpt' table unavailable
-    c_temp = conn.cursor()
+    c_temp = con.cursor()
     try:
-        c_temp.execute("SELECT stk_cpt from stk_cpt WHERE stk_num=%s"%stk_num)
+        c_temp.execute("SELECT stk_cpt from stk_cpt WHERE stk_num=%s",(stk_num,))
     except:
         stk_cpt_check = "no such table: stk_cpt"    
     temp = c_temp.fetchall()
@@ -147,16 +161,20 @@ for stk_lst in c.execute("SELECT stk_num,stk_name from stk_lst"):
     else:
         stk_cpt = "N/A"
 
-    c_temp = conn.cursor()
-    c_temp.execute("SELECT year_on_year2 from CO_QR WHERE stk_num=%s AND QUARTER='%s'"%(stk_num,lYear+"-4Q"))
+    c_temp = con.cursor()
+    c_temp.execute("SELECT year_on_year2 from co_qr WHERE stk_num=%s AND quarter=%s", (stk_num,lYear+"-4Q",))
     temp = c_temp.fetchall()
     if len(temp)>0:
         lastyear_yoy = temp[0][0]
+        #The unit is %. So the raw data 100 means 100%.
+        lastyear_yoy = float(lastyear_yoy) / 100
+        print "lastyear_yoy", lastyear_yoy
     else:
         lastyear_yoy = "N/A"
+        print "lastyear_yoy", lastyear_yoy
 
-    c_temp = conn.cursor()
-    c_temp.execute("SELECT current_prc from stk_prc WHERE stk_num=%s"%stk_num)
+    c_temp = con.cursor()
+    c_temp.execute("SELECT current_prc from stk_prc WHERE stk_num=%s",(stk_num,))
     temp = c_temp.fetchall()
     if len(temp)>0:
         current_prc = temp[0][0]
@@ -164,9 +182,10 @@ for stk_lst in c.execute("SELECT stk_num,stk_name from stk_lst"):
         current_prc = "N/A"
 
 #'stk_idt' table unavailable 
-    c_temp = conn.cursor()
+    '''
+    c_temp = con.cursor()
     try:
-        c_temp.execute("SELECT avg_pe from stk_idt WHERE stk_num=%s"%stk_num)
+        c_temp.execute("SELECT avg_pe from stk_idt WHERE stk_num=%s",(stk_num,))
     except:
         stk_idt_check = "no such table: stk_idt"
     temp = c_temp.fetchall()
@@ -174,21 +193,28 @@ for stk_lst in c.execute("SELECT stk_num,stk_name from stk_lst"):
         avg_pe = temp[0][0]
     else:
         avg_pe = "N/A"
-
+    '''
+    
+    avg_pe = "N/A"
+    
     #排除上年4Q无数据股票
     if R_lYear_4Q != "N/A":
         #使用3Q计算
         if R_tYear_3Q != "N/A":
             if R_lYear_3Q != "N/A":
-                if R_lYear_3Q != "0":
-                    FR_tYear_4Q = int(R_lYear_4Q) / int(R_lYear_3Q) * int(R_tYear_3Q)
+                if R_lYear_3Q != 0:
+                    print "R_lYear_3Q",R_lYear_3Q
+                    print "R_tYear_3Q",R_tYear_3Q
+                    print "R_lYear_4Q",R_lYear_4Q
+                    
+                    FR_tYear_4Q = float(R_lYear_4Q) / float(R_lYear_3Q) * float(R_tYear_3Q)
                 else:
                     FR_tYear_4Q = "N/A"
             else:
                 FR_tYear_4Q = "N/A"
                 
             if P_lYear_3Q != "N/A":
-                if P_lYear_3Q != "0":
+                if P_lYear_3Q != 0:
                     FP_tYear_4Q = P_lYear_4Q / P_lYear_3Q * P_tYear_3Q
                 else:
                     FP_tYear_4Q = "N/A"
@@ -198,7 +224,7 @@ for stk_lst in c.execute("SELECT stk_num,stk_name from stk_lst"):
         #使用2Q计算
         elif R_tYear_2Q != "N/A":
             if R_lYear_2Q != "N/A":
-                if R_lYear_2Q != "0":
+                if R_lYear_2Q != 0:
                     FR_tYear_4Q = R_lYear_4Q / R_lYear_2Q * R_tYear_2Q
                 else:
                     FR_tYear_4Q = "N/A"
@@ -206,7 +232,7 @@ for stk_lst in c.execute("SELECT stk_num,stk_name from stk_lst"):
                 FR_tYear_4Q = "N/A"
                 
             if P_lYear_2Q != "N/A":
-                if P_lYear_2Q != "0":
+                if P_lYear_2Q != 0:
                     FP_tYear_4Q = P_lYear_4Q / P_lYear_2Q * P_tYear_2Q
                 else:
                     FP_tYear_4Q = "N/A"
@@ -216,7 +242,7 @@ for stk_lst in c.execute("SELECT stk_num,stk_name from stk_lst"):
         #使用1Q计算
         elif R_tYear_1Q != "N/A":
             if R_lYear_1Q != "N/A":
-                if R_lYear_1Q != "0":
+                if R_lYear_1Q != 0:
                     FR_tYear_4Q = R_lYear_4Q / R_lYear_1Q * R_tYear_1Q
                 else:
                     FR_tYear_4Q = "N/A"
@@ -224,7 +250,7 @@ for stk_lst in c.execute("SELECT stk_num,stk_name from stk_lst"):
                 FR_tYear_4Q = "N/A"
                 
             if P_lYear_1Q != "N/A":
-                if P_lYear_1Q != "0":
+                if P_lYear_1Q != 0:
                     FP_tYear_4Q = P_lYear_4Q / P_lYear_1Q * P_tYear_1Q
                 else:
                     FP_tYear_4Q = "N/A"
@@ -240,60 +266,41 @@ for stk_lst in c.execute("SELECT stk_num,stk_name from stk_lst"):
 
     #计算财报预测EPS
     if FP_tYear_4Q != "N/A" and stk_cpt != "N/A":
-        forecast_EPS = "%i"%(int(FP_tYear_4Q) / int(stk_cpt))
+        forecast_EPS = "%.2f"%(float(FP_tYear_4Q) / float(stk_cpt))
     else:
         forecast_EPS = "N/A"
 
     #计算季报预测PE
-    if current_prc != "N/A" and forecast_EPS != "N/A":
-        forecast_PE = "%i"%(int(current_prc) / int(forecast_EPS))
+    if current_prc != "N/A" and forecast_EPS != "N/A" and float(forecast_EPS) != 0.0:
+        print "forecast_EPS",forecast_EPS
+        #print type(forecast_EPS)
+        forecast_PE = "%.2f"%(float(current_prc) / float(forecast_EPS))
     else:
         forecast_PE = "N/A"
 
     #计算目标价位
     if avg_pe != "N/A" and forecast_EPS != "N/A":
-        target_price = "%i"%(int(avg_pe) * int(forecast_EPS))
+        target_price = "%.2f"%(float(avg_pe) * float(forecast_EPS))
+        print "target_price",target_price
     else:
         target_price = "N/A"
+        print "target_price",target_price
 
-    insert_sql = '''INSERT OR REPLACE INTO cal_co_qr(    
-               stk_num, stk_name,
-               %s ,%s ,P_rate_1Q,
-               %s ,%s ,P_rate_2Q,
-               %s ,%s ,P_rate_3Q,
-               %s ,%s ,P_rate_4Q,
-               %s ,%s ,P_rate_5Q,
-               %s ,%s ,P_rate_6Q,
-               %s ,%s ,P_rate_7Q,
-               %s ,%s ,
-               %s ,%s ,
-               forecast_EPS ,
-               lastyear_yoy ,
-               forecast_PE ,
-               target_price 
-               ) VALUES('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')'''%("R_"+lYear+"_1Q","P_"+lYear+"_1Q",                                                                                           
-                     "R_"+lYear+"_2Q","P_"+lYear+"_2Q",
-                     "R_"+lYear+"_3Q","P_"+lYear+"_3Q",
-                     "R_"+lYear+"_4Q","P_"+lYear+"_4Q",
-                     "R_"+tYear+"_1Q","P_"+tYear+"_1Q",
-                     "R_"+tYear+"_2Q","P_"+tYear+"_2Q",
-                     "R_"+tYear+"_3Q","P_"+tYear+"_3Q",
-                     "FR_"+tYear+"_4Q","FP_"+tYear+"_4Q",
-                     "R_"+tYear+"_4Q","P_"+tYear+"_4Q",
-                     stk_num,stk_name,
-                     R_lYear_1Q,P_lYear_1Q,P_rate_1Q,
-                     R_lYear_2Q,P_lYear_2Q,P_rate_2Q,
-                     R_lYear_3Q,P_lYear_3Q,P_rate_3Q,
-                     R_lYear_4Q,P_lYear_4Q,P_rate_4Q,
-                     R_tYear_1Q,P_tYear_1Q,P_rate_5Q,
-                     R_tYear_2Q,P_tYear_2Q,P_rate_6Q,
-                     R_tYear_3Q,P_tYear_3Q,P_rate_7Q,
-                     FR_tYear_4Q,FP_tYear_4Q,
-                     R_tYear_4Q,P_tYear_4Q,
-                     forecast_EPS,lastyear_yoy,forecast_PE,target_price)
+    sql_forehead = '''REPLACE INTO cal_co_qr(stk_num,stk_name,%s,%s,P_rate_1Q,%s,%s,P_rate_2Q,%s,%s,P_rate_3Q,%s,%s,P_rate_4Q,%s,%s,P_rate_5Q,%s,%s,P_rate_6Q,%s,%s,P_rate_7Q,%s,%s,%s,%s,forecast_EPS,lastyear_yoy,forecast_PE,target_price)''' %("R_"+lYear+"_1Q","P_"+lYear+"_1Q","R_"+lYear+"_2Q","P_"+lYear+"_2Q","R_"+lYear+"_3Q","P_"+lYear+"_3Q","R_"+lYear+"_4Q","P_"+lYear+"_4Q","R_"+tYear+"_1Q","P_"+tYear+"_1Q","R_"+tYear+"_2Q","P_"+tYear+"_2Q","R_"+tYear+"_3Q","P_"+tYear+"_3Q","FR_"+tYear+"_4Q","FP_"+tYear+"_4Q","R_"+tYear+"_4Q","P_"+tYear+"_4Q")
     #print insert_sql
-    conn.execute(insert_sql)
-    conn.commit()
+    
+    #print sql_forehead
+    
+    sql_tail = '''VALUES ("%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s")''' %(stk_num,stk_name,R_lYear_1Q,P_lYear_1Q,P_rate_1Q,R_lYear_2Q,P_lYear_2Q,P_rate_2Q,R_lYear_3Q,P_lYear_3Q,P_rate_3Q,R_lYear_4Q,P_lYear_4Q,P_rate_4Q,R_tYear_1Q,P_tYear_1Q,P_rate_5Q,R_tYear_2Q,P_tYear_2Q,P_rate_6Q,R_tYear_3Q,P_tYear_3Q,P_rate_7Q,FR_tYear_4Q,FP_tYear_4Q,R_tYear_4Q,P_tYear_4Q,forecast_EPS,lastyear_yoy,forecast_PE,target_price)
+    
+    #print sql_tail
+    
+    insert_sql = sql_forehead + sql_tail
+    
+    #print insert_sql
+    
+    c.execute(insert_sql)
+    con.commit()
         
 c.close()
 print stk_cpt_check
